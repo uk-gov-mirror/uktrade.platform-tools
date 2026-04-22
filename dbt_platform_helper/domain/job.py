dd1fc620408fa416e56b476d712420a79d4e60f0
YacquubAdan <yacquub.adan@digital.trade.gov.uk>
2026-04-22 20:01:37 +0100

    feat: add scheduled job domain

---
+++ dbt_platform_helper/domain/job.py
@@ -0,0 +1,16 @@
+from dbt_platform_helper.providers.step_functions import StepFunctions
+from dbt_platform_helper.providers.io import ClickIOProvider
+
+class JobManager:
+    def __init__(self, sfn_provider: StepFunctions, io: ClickIOProvider = ClickIOProvider()):
+        self.sfn_provider = sfn_provider
+        self.io = io
+    
+    def run(self, app: str, env: str, name: str):
+      
+      self.io.info(f"Looking up state machine for job '{name}' in {app}/{env}...")
+      state_machine_arn = self.sfn_provider.find_state_machine_arn(name)
+      
+      response = self.sfn_provider.start_execution(state_machine_arn)
+      self.io.info(f"Execution started:  {response['executionArn']}")
+    
