f10fa9c2c13f7961cae12a10fd5d099d76ea0fdd
YacquubAdan <yacquub.adan@digital.trade.gov.uk>
2026-04-22 20:02:00 +0100

    feat: add `job run` command

---
+++ dbt_platform_helper/commands/job.py
@@ -0,0 +1,43 @@
+import click
+
+from dbt_platform_helper.domain.job import JobManager
+from dbt_platform_helper.platform_exception import PlatformException
+from dbt_platform_helper.providers.io import ClickIOProvider
+from dbt_platform_helper.providers.step_functions import StepFunctions
+from dbt_platform_helper.utils.application import (
+    ApplicationEnvironmentNotFoundException,
+)
+from dbt_platform_helper.utils.application import load_application
+from dbt_platform_helper.utils.click import ClickDocOptGroup
+
+
+@click.group(cls=ClickDocOptGroup)
+def job():
+    """Commands for managing a scheduled job."""
+    
+@job.command()
+@click.option("--app", "-a", help="Application name", required=True)
+@click.option("--env", "-e", help="Environment name", required=True)
+@click.option("--name", "-n", help="Name of the scheduled job", required=True)
+def run(app: str, env: str, name: str):
+    """Runs a scheduled job on demand."""
+    
+    try:
+        application = load_application(app=app, env=env)
+
+        # TODO This is a workaround until DBTP-2754 is fixed
+        try:
+            sfn_client = application.environments[env].session.client("stepfunctions")
+        except KeyError:
+            raise ApplicationEnvironmentNotFoundException(app, env)
+
+        sfn_provider: StepFunctions = StepFunctions(
+            sfn_client,
+            application.name,
+            env,
+        )
+
+        JobManager(sfn_provider=sfn_provider).run(app, env, name)
+    except PlatformException as err:
+        ClickIOProvider().abort_with_error(str(err))
+        
